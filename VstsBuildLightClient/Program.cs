85a70adc6cc920a7a62fb06fcbcd799726879c74
Raph <raphael@haddad.net.au>
2017-06-29 11:39:49 +1000

    modify program to get latest build from VSTS and output to a build light whether it has been successful or not

--- VstsBuildLightClient/Program.cs
+++ VstsBuildLightClient/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
-using System.Net;
-using Microsoft.Extensions.Configuration;
+using System.Threading;
 using Microsoft.TeamFoundation.Build.WebApi;
 using Microsoft.TeamFoundation.Core.WebApi;
 using Microsoft.VisualStudio.Services.Common;
-using Microsoft.VisualStudio.Services.OAuth;
 using Microsoft.VisualStudio.Services.WebApi;
-using Microsoft.VisualStudio.Services;
 
 namespace VstsBuildLightClient
 {
@@ -24,16 +20,77 @@ namespace VstsBuildLightClient
             var projectClient = client.GetClient<ProjectHttpClient>();
             var project = projectClient.GetProjects().Result
                                                      .Single(x => x.Name == config.Project);
+            var buildLight = BuildLight.Initialise(config.BuildLight);
 
-            var lastBuildResult = buildClient.GetBuildsAsync(project.Id)
-                                             .Result
-                                             .OrderBy(x => x.FinishTime)
-                                             .Last()
-                                             .Result;
+            while (true)
+            {
+                var lastBuildResult = buildClient.GetBuildsAsync(project.Id)
+                                                 .Result
+                                                 .OrderBy(x => x.FinishTime)
+                                                 .Last()
+                                                 .Result;
 
 
-            Console.Read();
+                switch (lastBuildResult)
+                {
+                    case BuildResult.Succeeded:
+                        buildLight.ChangeColourToGreen();
+                        break;
+                    case BuildResult.PartiallySucceeded:
+                        buildLight.ChangeColourToAmber();
+                        break;
+                    default:
+                        buildLight.ChangeColourToRed();
+                        break;
+                }
+                Thread.Sleep(2000);
+            }
         }
 
+    }
+
+    internal abstract class BuildLight
+    {
+        internal virtual void ChangeColourToRed()
+        {
+            
+        }
+
+        internal virtual void ChangeColourToGreen()
+        {
+
+        }
+
+        internal virtual void ChangeColourToAmber()
+        {
+
+        }
+
+        internal static BuildLight Initialise(VstsConfiguration.BuildLights buildLight)
+        {
+            if (buildLight == VstsConfiguration.BuildLights.Console)
+            {
+                return new ConsoleBuildLight();
+            }
+            return null;
+        }
+    }
+
+    internal class ConsoleBuildLight : BuildLight
+    {
+        internal override void ChangeColourToGreen()
+        {
+            Console.WriteLine("Build is good");
+        }
+
+        internal override void ChangeColourToAmber()
+        {
+            Console.WriteLine("Build is partially successful");
+        }
+
+        internal override void ChangeColourToRed()
+        {
+            Console.WriteLine("Build has failed");
+        }
     }
 }
--- VstsBuildLightClient/VstsConfiguration.cs
+++ VstsBuildLightClient/VstsConfiguration.cs
@@ -1,11 +1,12 @@
+using System;
 using System.IO;
 using Microsoft.Extensions.Configuration;
 
 namespace VstsBuildLightClient
 {
-    public class VstsConfiguration
+    internal class VstsConfiguration
     {
-        public VstsConfiguration()
+        internal VstsConfiguration()
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -15,10 +16,32 @@ namespace VstsBuildLightClient
             Url = config["vsts_url"];
             PersonalKey = config["vsts_ personal_key"];
             Project = config["vsts_project"];
+            var buildLight = config["build_light"];
+
+            if (buildLight.Equals("console", StringComparison.InvariantCultureIgnoreCase))
+            {
+                BuildLight = BuildLights.Console;
+            }
+            else if (buildLight.Equals("delcom", StringComparison.InvariantCultureIgnoreCase))
+            {
+                BuildLight = BuildLights.Delcom;
+            }
+            else
+            {
+                throw new ArgumentException($"{buildLight} is not a valid Build Light value in settings");
+            }
         }
 
-        public string Url { get; } 
-        public string PersonalKey { get; }
-        public string Project { get; }
+        internal string Url { get; } 
+        internal string PersonalKey { get; }
+        internal string Project { get; }
+        internal BuildLights BuildLight { get; }
+
+        internal enum BuildLights
+        {
+            Console,
+            Delcom,
+            Kuanda
+        }
     }
 }
